65ea543de7462908709811cfc3d11d11e63e6988
Yildiray <y.ylimaz1976@gmail.com>
2021-06-12 20:18:02 +0300

    files added and modifed

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,13 +4,12 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-newgrp docker
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 mkdir -p /home/ec2-user/bookstore-api
-TOKEN="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"
-FOLDER="https://$TOKEN@raw.github.com/YY4411/203-dockerization-bookstore-api-on-python-flask-mysql/"
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+FOLDER="https://$TOKEN@raw.githubusercontent.com/clarusway/cw-workshop/dev/devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
